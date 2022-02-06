b2d1f6debc59fcee8360da00c1a473a2b0a46ced
Ayub Mohamud <a.mohamud1@hotmail.com>
2022-02-06 19:39:12 +0000

    install packages and finish navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import NavBar from './Components/NavBar.jsx';
+// import Home from './sections/Home';
+// import Projects from './sections/Projects';
+// import Blogs from './sections/Blogs';
+// import Contact from './sections/Contact';
+// import Footer from './components/Footer';
+// import { animateScroll as scroll } from 'react-scroll';
+// import { FiArrowUp } from 'react-icons/fi';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [visible, setVisible] = useState(false);
+
+    const toggleVisible = () => {
+        const scrolled = document.documentElement.scrollTop;
+        if (scrolled > 250) {
+            setVisible(true);
+        } else if (scrolled <= 250) {
+            setVisible(false);
+        }
+    };
+
+    window.addEventListener('scroll', toggleVisible);
+
+    return (
+        <>
+            <NavBar />
+            {/* <main className="main-container">
+                <Home />
+                <Projects />
+                <Blogs />
+                <Contact />
+            </main> */}
+            {/* <Footer /> */}
+
+            {/* --------SCROLL TO TOP BUTTON-------- */}
+        </>
+    );
 }
 
 export default App;
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,116 @@
+import { Link } from 'react-scroll';
+import { FiMenu } from 'react-icons/fi';
+import { useState } from 'react';
+
+const NavBar = () => {
+    const [showMenu, setShowMenu] = useState(false);
+
+    return (
+        <nav className="navbar">
+            <div className="hamburger-menu">
+                <FiMenu
+                    style={{ fontSize: '2rem' }}
+                    onClick={() => setShowMenu(!showMenu)}
+                />
+            </div>
+
+            <div className={`${!showMenu && 'hide-menu'} navbar-links`}>
+                {!showMenu ? (
+                    // --------------LINKS FOR DESKTOP--------------
+                    <>
+                        <Link to="home" smooth={true} duration={1000}>
+                            <p
+                                className="nav-link"
+                                onClick={() => setShowMenu(false)}
+                            >
+                                Home
+                            </p>
+                        </Link>
+                        <Link to="projects" smooth={true} duration={1000}>
+                            <p
+                                className="nav-link"
+                                onClick={() => setShowMenu(false)}
+                            >
+                                Projects
+                            </p>
+                        </Link>
+                        <Link to="blogs" smooth={true} duration={1000}>
+                            <p
+                                className="nav-link"
+                                onClick={() => setShowMenu(false)}
+                            >
+                                Blogs
+                            </p>
+                        </Link>
+                        <Link to="contact" smooth={true} duration={1000}>
+                            <p
+                                className="nav-link"
+                                onClick={() => setShowMenu(false)}
+                            >
+                                Contact
+                            </p>
+                        </Link>
+                    </>
+                ) : (
+                    // ----------------LINKS FOR MOBILE----------------
+                    <>
+                        <Link
+                            to="home"
+                            offset={-250}
+                            smooth={true}
+                            duration={1000}
+                        >
+                            <p
+                                className="nav-link"
+                                onClick={() => setShowMenu(false)}
+                            >
+                                Home
+                            </p>
+                        </Link>
+                        <Link
+                            to="projects"
+                            offset={-230}
+                            smooth={true}
+                            duration={1000}
+                        >
+                            <p
+                                className="nav-link"
+                                onClick={() => setShowMenu(false)}
+                            >
+                                Projects
+                            </p>
+                        </Link>
+                        <Link
+                            to="blogs"
+                            offset={-230}
+                            smooth={true}
+                            duration={1000}
+                        >
+                            <p
+                                className="nav-link"
+                                onClick={() => setShowMenu(false)}
+                            >
+                                Blogs
+                            </p>
+                        </Link>
+                        <Link
+                            to="contact"
+                            offset={-230}
+                            smooth={true}
+                            duration={1000}
+                        >
+                            <p
+                                className="nav-link"
+                                onClick={() => setShowMenu(false)}
+                            >
+                                Contact
+                            </p>
+                        </Link>
+                    </>
+                )}
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
